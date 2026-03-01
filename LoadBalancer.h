112f099afb1dbd61cd32ad204f0b223410aae38c
Ethan O'Brien <gameguy0308@gmail.com>
2026-03-01 16:16:45 -0600

    LoadBalancer header update

--- LoadBalancer.h
+++ LoadBalancer.h
@@ -1,9 +1,25 @@
 #define LOADBALANCER_H
 #include "Webserver.h"
 #include "Request.h"
+#include <queue>
+#include <vector>
+#include <string>
 
 class LoadBalancer{
     private:
-    int numServers;
+        int numServers;
+        std::vector<Webserver> servers;
+        std::vector<std::queue<Request>> requestQueues;
+        std::vector<int> queuedTime;
+        std::vector<std::string> blockedIPs;
     public:
+        LoadBalancer(int numServers);
+        void receiveRequest(Request r);
+        void tick();
+        void checkFirewall(Request r);
+        void takeDownServer();
+        void bringUpServer();
+        bool serverNeeded();
+        bool serverIdle();
+
 };
